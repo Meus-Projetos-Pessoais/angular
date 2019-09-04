bb76aa5087c6f9fe9af729a1b7933aef81ce1b2f
Andre Emidio <andresjc2008@gmail.com>
2019-09-04 10:35:56 -0300

    Criação do servidor Nodejs

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express()
+
+app.listen(3000, function(){
+
+    console.log('Rodando.')
+}
+);
+
+app.set('View egiee', 'ejs');
+
+app.get('/', (req, res) => {
+
+    res.render('index.ejs')
+});
